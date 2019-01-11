69b6f819abfd5b0b3d7b8faf748b321b3f67ca1a
Louis345 <jt125845@gmail.com>
2019-01-10 22:02:33 -0500

    modal is now a component

--- src/components/Widgets/Modal.jsx
+++ src/components/Widgets/Modal.jsx
@@ -1,25 +1,15 @@
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button } from 'react-bootstrap';
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class ModalWidget extends Component {
   constructor(props, context) {
     super(props, context);
-
-    this.handleShow = this.handleShow.bind(this);
     this.handleClose = this.handleClose.bind(this);
-
-    this.state = {
-      show: false
-    };
   }
 
   handleClose() {
-    this.setState({ show: false });
-  }
-
-  handleShow() {
-    this.setState({ show: true });
+    this.props.closeModal();
   }
 
   render() {
