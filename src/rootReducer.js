6f7521555351823d8fa7dba06e1d8160a921aa05
William Peirce <bill@seeyourcampsite.com>
2019-01-14 11:36:27 -0500

    Reorganized reducers. Removed some unnecessary files and action types.

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,18 +3,14 @@ import {
 } from "redux";
 import LoginReducer from "./components/Login/LoginReducer";
 import sharedReducer from "./sharedReducer";
-import SearchReducer from "./components/Search/SearchReducer";
 import EditProfileReducer from "./components/EditProfile/EditProfileReducer";
 import NewProfileReducer from "./components/NewProfile/NewProfileReducer";
-import ViewProfileReducer from "./components/ViewProfile/ViewProfileReducer";
 
 const rootReducer = combineReducers({
   Shared: sharedReducer,
   Login: LoginReducer,
-  Search: SearchReducer,
   EditProfile: EditProfileReducer,
-  NewProfile: NewProfileReducer,
-  ViewProfile: ViewProfileReducer
+  NewProfile: NewProfileReducer
 });
 
 export default rootReducer;
